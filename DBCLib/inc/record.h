87e278d060bf727e0492c09ef39f5a9407d19d86
Alexander <37180898+MrSago@users.noreply.github.com>
2023-04-15 01:50:45 +0300

    add dbc src files

---
+++ DBCLib/inc/record.h
@@ -0,0 +1,36 @@
+
+#ifndef _RECORD_H_
+#define _RECORD_H_
+
+#include <cstdint>
+#include <string>
+
+#include "dbc.h"
+
+class DBCHandler;
+struct DBCFile;
+
+class Record {
+ public:
+  float GetFloat(uint32_t field) const;
+  void SetFloat(uint32_t field, float val) const;
+
+  uint32_t GetUInt32(uint32_t field) const;
+  void SetUInt32(uint32_t field, uint32_t val) const;
+
+  std::string GetString(uint32_t field) const;
+  void SetStringRef(uint32_t field, const char* str) const;
+  void AddStringRef(uint32_t field, const char* str) const;
+
+ private:
+  Record(DBCFile* dbc, uint32_t index);
+
+  void ValidateIndex(uint32_t field) const;
+
+  DBCFile* dbc_;
+  char* pointer_;
+
+  friend class DBCHandler;
+};
+
+#endif  // _RECORD_H_
---
+++ DBCLib/src/record.cc
@@ -0,0 +1,65 @@
+
+#include "record.h"
+
+#include <cstdint>
+#include <cstring>
+#include <stdexcept>
+#include <string>
+
+float Record::GetFloat(uint32_t field) const {
+  uint32_t index = field - 1;
+  ValidateIndex(index);
+  return *reinterpret_cast<float*>(pointer_ + sizeof(uint32_t) * index);
+}
+
+void Record::SetFloat(uint32_t field, float val) const {
+  uint32_t index = field - 1;
+  ValidateIndex(index);
+  *reinterpret_cast<float*>(pointer_ + sizeof(uint32_t) * index) = val;
+}
+
+uint32_t Record::GetUInt32(uint32_t field) const {
+  uint32_t index = field - 1;
+  ValidateIndex(index);
+  return *reinterpret_cast<uint32_t*>(pointer_ + sizeof(uint32_t) * index);
+}
+
+void Record::SetUInt32(uint32_t field, uint32_t val) const {
+  uint32_t index = field - 1;
+  ValidateIndex(index);
+  *reinterpret_cast<uint32_t*>(pointer_ + sizeof(uint32_t) * index) = val;
+}
+
+std::string Record::GetString(uint32_t field) const {
+  ValidateIndex(field - 1);
+  stringref offset = GetUInt32(field);
+  return std::string(dbc_->string_block + offset);
+}
+
+void Record::SetStringRef(uint32_t field, const char* str) const {
+  ValidateIndex(field - 1);
+  stringref offset = GetUInt32(field);
+  char* string_block_pointer = dbc_->string_block + offset;
+  strcpy(string_block_pointer, str);
+}
+
+void Record::AddStringRef(uint32_t field, const char* str) const {
+  ValidateIndex(field - 1);
+  stringref offset = dbc_->header.string_block_size;
+  SetUInt32(field, offset);
+
+  dbc_->header.string_block_size += strlen(str) + 1;
+  dbc_->string_block = reinterpret_cast<char*>(
+      realloc(dbc_->string_block, dbc_->header.string_block_size));
+
+  char* string_block_pointer = dbc_->string_block + offset;
+  strcpy(string_block_pointer, str);
+}
+
+Record::Record(DBCFile* dbc, uint32_t index)
+    : dbc_(dbc), pointer_(dbc->records + dbc->header.record_size * index) {}
+
+void Record::ValidateIndex(uint32_t index) const {
+  if (index < 0 || index >= dbc_->header.field_count)
+    throw std::out_of_range("Field index out of range");
+}
