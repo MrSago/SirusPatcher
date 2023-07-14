f128c504c35ab7410f207ed5cf9bdfc7dab9a464
Alexander <37180898+MrSago@users.noreply.github.com>
2023-07-14 20:38:16 +0300

    Revert "updated ysera"
    This reverts commit cf65ff7382c538d4cf6349633e2ba52e4e0a9081.

--- QtGui/yserachanger.cc
+++ QtGui/yserachanger.cc
@@ -7,25 +7,15 @@
 #include "DBC/record.h"
 
 void YseraChanger::Change() {
-  const std::string kVisualEffectNameDbcPath = "./SpellVisualEffectName.dbc";
-  const std::string kVisualEffectNameDbcPathSave =
-      "./SpellVisualEffectName.dbc.save";
+  const std::string kDbcPath = "./SpellVisualEffectName.dbc";
+  const std::string kDbcPathSave = "./SpellVisualEffectName.dbc.save";
 
-  const std::string kVisualDbcPath = "./SpellVisual.dbc";
-  const std::string kVisualDbcPathSave = "./SpellVisual.dbc.save";
-
-  const std::string kVisualKitDbcPath = "./SpellVisualKit.dbc";
-  const std::string kVisualKitDbcPathSave = "./SpellVisualKit.dbc.save";
-
-  DBCHandler visual_effect_name, visual, visual_kit;
-  if (visual_effect_name.Load(kVisualEffectNameDbcPathSave) !=
-          DBCError::kSuccess ||
-      visual.Load(kVisualDbcPathSave) != DBCError::kSuccess ||
-      visual_kit.Load(kVisualKitDbcPathSave) != DBCError::kSuccess) {
+  DBCHandler handler;
+  if (handler.Load(kDbcPathSave) != DBCError::kSuccess) {
     throw std::runtime_error("gg");
   }
 
-  Record ysera = visual_effect_name.GetRecordById(8797);
+  Record ysera = handler.GetRecordById(8797);
   ysera.AddStringRef(
       3, "Spells\\Stranglethorn_diamondmines_circle_effect_solid_green.mdx");
   ysera.SetFloat(4, 4.15f);
@@ -33,19 +23,7 @@ void YseraChanger::Change() {
   ysera.SetFloat(6, 4.15f);
   ysera.SetFloat(7, 4.15f);
 
-  Record ysera_visual = visual.GetRecordById(19155);
-  unsigned int tmp = ysera_visual.GetUInt32(26);
-  ysera_visual.SetUInt32(26, 0);
-  ysera_visual.SetUInt32(5, tmp);
-
-  Record ysera_kit = visual_kit.GetRecordById(18001);
-  tmp = ysera_kit.GetUInt32(15);
-  ysera_kit.SetUInt32(15, 0);
-  ysera_kit.SetUInt32(6, tmp);
-
-  if (visual_effect_name.Save(kVisualEffectNameDbcPath) != DBCError::kSuccess ||
-      visual.Save(kVisualDbcPath) != DBCError::kSuccess ||
-      visual_kit.Save(kVisualKitDbcPath) != DBCError::kSuccess) {
+  if (handler.Save(kDbcPath) != DBCError::kSuccess) {
     throw std::runtime_error("wp");
   }
 }
